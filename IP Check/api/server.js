2c39e711d53b5e3df0b51fd4b7a97f3dc14c1b42
Iago Felipe <gagererk@gmail.com>
2026-04-02 17:49:19 -0300

    Update CORS origin and maintain existing functionality

--- IP Check/api/server.js
+++ IP Check/api/server.js
@@ -4,7 +4,7 @@ const rateLimit = require('@fastify/rate-limit');
 const net = require('net');
 
 fastify.register(cors, {
-    origin: 'https://project-tzws4.vercel.app/api/server.js' 
+    origin: '/api/server.js' 
 });
 
 fastify.register(rateLimit, {
